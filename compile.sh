57e77c254301ab2d674b9fb6f8f8baabc6999391
Plattfisken <158032673+Plattfisken@users.noreply.github.com>
2025-05-13 16:28:48 -0300

    compile.sh behövs inte längre

--- compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-dotnet run code.c && \
-as -arch arm64 -o code.o code.s && \
-ld -arch arm64 -o a.out -lSystem -syslibroot `xcrun -sdk macosx --show-sdk-path` code.o && \
-rm code.s code.o
